d631ca90858105aa4d55857a80cb431eeb1ffeec
christiandrochon <khrisys@protonmail.com>
2024-07-22 23:15:41 +0200

    ajout icon sur tel et devis gratuit sur header

--- src/components/MasterHeader.js
+++ src/components/MasterHeader.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 class Header extends Component {
     render() {
         return (
             <header className="masthead">
                 <div className="container">
-                    <div className="masthead-subheading">Talais</div>
+                    {/*<div className="masthead-subheading">Maçonnerie, Charpente et couverture, Piscine, Tailleur de pierre</div>*/}
+                    <div className="masthead-subheading">Neuf et rénovation</div>
                     <div className="masthead-heading text-uppercase">Guillemot et frères</div>
-                    <a className="btn btn-primary btn-xl text-uppercase" href="#services">Devis gratuit</a>
+                    <div className="d-lg-inline-flex" style={{justifyContent: 'center', gap: '5em'}}>
+                        <div className="masthead-heading text-uppercase">
+                            <a className="btn btn-secondary btn-xl text-uppercase" href="#">
+                                <DescriptionIcon style={{marginRight: 20, marginBottom: 2}}/>Devis gratuit
+                            </a>
+                        </div>
+                        <div className="masthead-heading text-uppercase">
+                            <a className="btn btn-secondary btn-xl text-uppercase" href="#">
+                                <LocalPhoneIcon style={{marginRight: 20, marginBottom: 2}}/>06 78 27 11 88
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </header>
-        );
+        )
+            ;
     }
 }
 
