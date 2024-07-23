68d470a171ab18cfce2d000e53fde745e325aa67
christiandrochon <khrisys@protonmail.com>
2024-07-23 18:55:28 +0200

    affichage plus clair des contacts et interaction avec tel et mail

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,12 +6,16 @@ class Contact extends Component {
         return (
             <section className="page-section bg-light" id="portfolio">
                 <div className="container">
-                    <div className="text-center">
+                    <div className="text-center containerAvecMarges">
                         <h2 className="section-heading text-uppercase">Contact</h2>
-                        {/*<h3 className="section-subheading text-muted">todo.</h3>*/}
-                        <p>Vous pouvez nous contacter par téléphone au 06 78 27 11 88</p>
-                        <p>Vous pouvez nous contacter par mail à l'adresse suivante: guillemot.freres@live.fr </p>
-                        <p>Vous pouvez nous contacter par courrier à l'adresse suivante: 27, rue du onze novembre 1918, 33590 TALAIS</p>
+                        <h3 className="section-subheading text-muted">Nous sommes là pour vous aider.</h3>
+                        <p className="pb-4">Cliquez pour nous appeler au <a className="mailto" href="tel:+33678271188"><h2>06 78 27 11 88</h2></a>
+                        </p>
+                        <p className="pb-5">Cliquez pour nous envoyer un email à <a className="mailto"
+                                                                                    href="mailto:guillemot.freres@live.fr"><h2>guillemot.freres@live.fr</h2></a>
+                        </p>
+
+                        <p>Adresse de la société : <br/><h2><strong>27, rue du onze novembre 1918 <br/> 33590 TALAIS</strong></h2></p>
                         {/*<p>Vous pouvez nous contacter par le formulaire de contact ci-dessous: </p>*/}
                         {/*<p>Vous pouvez nous contacter par le chat en ligne ci-dessous: </p>*/}
                         {/*<p>Vous pouvez nous contacter par le formulaire de devis en ligne ci-dessous: </p>*/}
@@ -22,8 +26,8 @@ class Contact extends Component {
                         {/*<p>Vous pouvez nous contacter par le formulaire de demande de contact en ligne ci-dessous: </p>*/}
 
 
-
-                        <svg><DashboardRoundedIcon/></svg>
+                        <svg className="my-5"><DashboardRoundedIcon/></svg>
+                        <img src="/template/assets/img/contact-us-banner.jpeg" alt="contact" className="img-fluid"/>
                     </div>
                 </div>
             </section>
